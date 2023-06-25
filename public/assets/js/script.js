68e07e4a40c1c5db6fb82f39e4c3031d8316dfdf
codesyariah122 <pujiermanto@gmail.com>
2023-06-25 19:41:20 +0700

    update script

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -100,18 +100,16 @@ function isMobileDevice() {
 function domElement (data) {
   const blogSelector = document.querySelector('.blog-content')
   const newEl = document.createElement("div")
+
   if(isMobileDevice()) {
     newEl.classList.add("col-span-full")
   }
+
   newEl.innerHTML = `
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
     <img class="rounded-t-lg" src="https:${data.cover.fields.file.url}" alt="" />
-    </a>
     <div class="p-5">
-    <a href="#">
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${data.title}</h5>
-    </a>
     
     <a href="/blog/${data.slug}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
     Read more
