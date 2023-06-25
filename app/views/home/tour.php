570bc3fb7499856948dd7182397b91315fccfef9
codesyariah122 <pujiermanto@gmail.com>
2023-06-25 23:11:19 +0700

    tweaking crawlable indexing

--- app/views/home/tour.php
+++ app/views/home/tour.php
@@ -48,12 +48,7 @@
                     <span class="text-xl font-bold text-gray-900 dark:text-white">
                         Rp. <?=$tour['price']?>
                     </span>
-                    <a href="javascript:void(0)" onClick="orderNow({
-                        img: `<?=$tour['img']?>`, 
-                        title: `<?=$tour['title']?>`, 
-                        price: `<?=$tour['price']?>`, 
-                        category: `<?=$category?>`
-                    })" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pesan Sekarang</a>
+                    <a href="https://wa.me/<?=$data['phone']?>?text=<?=$helpers->format_wa_send(['img' => $tour['img'], 'title' => $tour['title'], 'price' => $tour['price'], 'category' => $category])?>" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pesan Sekarang</a>
                 </div>
             </div>
         </div>
