8c67adac2110bf16362b16231adf84b577cfebf7
codesyariah122 <pujiermanto@gmail.com>
2023-07-01 07:05:35 +0700

    added server run with bash

--- app/config/Router.php
+++ app/config/Router.php
@@ -19,40 +19,32 @@ class Router {
     public function get($route, $handler) {
        $route = str_replace('{slug}', '([^/]+)', $route);
 
-    // Menambahkan rute dan handler ke daftar rute
        $this->routes[$route] = $handler;
     }
 
     public function run() {
         $uri = $_SERVER['REQUEST_URI'];
 
-        // Menghapus query string dari URI
         $uri = strtok($uri, '?');
 
-        // Mencocokkan URI dengan daftar rute
         foreach ($this->routes as $route => $handler) {
-            // Mencocokkan pola rute dengan URI
             if (preg_match('#^' . $route . '$#', $uri, $matches)) {
-                // Menghapus elemen pertama ($matches[0]) yang berisi URI lengkap
+
                 array_shift($matches);
 
-                // Memisahkan handler menjadi nama kontroler dan metode
                 $handlerParts = explode('@', $handler);
                 $controllerName = $handlerParts[0];
                 $methodName = $handlerParts[1];
 
-                // Membuat instance kontroler
                 $controllerNamespace = 'app\controllers\\' . $controllerName;
                 $controller = new $controllerNamespace();
 
-                // Memanggil metode pada kontroler dengan argumen yang cocok dari URI
                 call_user_func_array([$controller, $methodName], $matches);
 
                 return;
             }
         }
 
-        // Jika tidak ada cocokan rute, tampilkan halaman 404
         http_response_code(404);
 
         header("HTTP/1.0 404 Not Found");
