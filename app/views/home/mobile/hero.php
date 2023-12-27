bb66f220487b8b2d45ecdfee9697721e502df723
root <root@dksindo.neo.internal>
2023-12-27 12:54:47 +0700

    update update

--- app/views/home/mobile/hero.php
+++ app/views/home/mobile/hero.php
@@ -1,34 +1,283 @@
+<style>
+    @font-face {
+        font-family: 'StorytellerScript';
+        src: url('/public/assets/fonts/StorytellerScript-Regular.otf') format('opentype');
+        font-weight: normal;
+        font-style: normal;
+    }
 
-<section id="home" class="bg-center bg-no-repeat bg-cover bg-[url('<?=$data['hero_img']?>')] bg-gray-700 bg-blend-multiply">
-    <div class="px-4 mx-auto max-w-screen-xl text-center py-24">
-        <a href="#rental" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800 mt-12">
-            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Sewa Mobil </span>&nbsp;<i class="fa-solid fa-angle-down text-lg"></i>
-        </a>
+    .hero {
+        width: 100%;
+        min-height: 750px;
+        position: relative;
+        top: 0;
+        left: 0;
+        background-color: #d9edfd;
+    }
 
-        <!-- Hero Title -->
-        <h1 class="mb-4 text-3xl lg:text-xl font-extrabold tracking-tight leading-none text-white leading-loose py-0"><?=$data['title']?></h1>
+    .hero-cta {
+        position: relative;
+        z-index: 3;
+        border: 2px solid rgba(82, 78, 183, 1.7);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-        <!-- Tagline -->
-        <h2 id="typing" class="mb-6 text-2xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl py-0"></h2>
 
-        <!-- Description tag -->
-        <h2 class="text-xl font-bold text-gray-300 lg:text-2xl sm:px-16 lg:px-48 leading-loose">
-            <?=$data['desc_title']?>
-        </h2>        
-        <p class="mb-8 text-md font-normal text-gray-300 lg:text-lg sm:px-16 lg:px-48 leading-loose py-2">
-            <?=$data['desc']?>
-        </p>
+    #typing-cta {
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    .layer-1 {
+        animation: parallax_fg linear 20s infinite both;
+        background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/24650/1.png) 0 100% repeat-x;
+        z-index: 2;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 136px;
+    }
+
+    .layer-2 {
+        animation: parallax_fg linear 30s infinite both;
+        background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/24650/2.png) 0 100% repeat-x;
+        z-index: 1;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 145px;
+    }
+
+    .layer-3 {
+        animation: parallax_fg linear 55s infinite both;
+        background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/24650/3.png) 0 100% repeat-x;
+        z-index: 1;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 158px;
+    }
+
+    .layer-4 {
+        animation: parallax_fg linear 75s infinite both;
+        background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/24650/4.png) 0 100% repeat-x;
+        z-index: 1;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 468px;
+    }
+
+    .layer-5 {
+        animation: parallax_fg linear 95s infinite both;
+        background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/24650/5.png) 0 100% repeat-x;
+        z-index: 1;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 311px;
+    }
+
+    .layer-6 {
+        animation: parallax_fg linear 120s infinite both;
+        background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/24650/6.png) 0 100% repeat-x;
+        z-index: 1;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 222px;
+    }
+
+    .bike-1 {
+        background: url(https://www.kallatoyota.co.id/storage/images/360exterior/16529289205-all-new-avanza.png) 0 100% no-repeat;
+        z-index: 1;
+        position: absolute;
+        bottom: 100px;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 75px;
+    }
+
+    .bike-2 {
+        background: url(https://www.kallatoyota.co.id/storage/images/360exterior/16529317835-all-new-hiace-premio.png) 0 100% no-repeat;
+        z-index: 1;
+        position: absolute;
+        bottom: 100px;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 75px;
+    }
+
+    .bike-3 {
+        background: url(https://resource.digitaldealer.com.au/image/104181539363ca00934a347251292065_0_0.png) 0 100% no-repeat;
+        z-index: 1;
+        position: absolute;
+        bottom: 100px;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 75px;
+    }
+
+    .bike-4 {
+        background: url(https://www.toyotasutan.com/product/alphard/img/360-view/data1/images/alfonso_360ext_06.png) 0 100% no-repeat;
+        z-index: 1;
+        position: absolute;
+        bottom: 100px;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: auto 75px;
+    }
+
+    .bike-1 {
+        animation: parallax_bike linear 10s infinite both;
+    }
+
+    .bike-2 {
+        animation: parallax_bike linear 15s infinite both;
+    }
+
+    .bike-3 {
+        animation: parallax_bike linear 20s infinite both;
+    }
+
+    .bike-4 {
+        animation: parallax_bike linear 25s infinite both;
+    }
+
+    @keyframes parallax_fg {
+        0% {
+            background-position: 2765px 100%;
+        }
+
+        100% {
+            background-position: 550px 100%;
+        }
+    }
+
+    @keyframes parallax_bike {
+        0% {
+            background-position: -300px 100%;
+        }
+
+        100% {
+            background-position: 2000px 100%;
+        }
+    }
+
+    .logo {
+        /* margin: 70px auto;
+        position: absolute; */
+        z-index: 2;
+        width: 100%;
+    }
+
+    .logo img {
+        display: block;
+        margin: 0 auto;
+        max-width: 100%;
+    }
+
+    .logo h1 {
+        font-family: 'Bungee Spice', sans-serif;
+        font-size: 4rem;
+    }
+
+    .logo h4 {
+        font-family: 'StorytellerScript';
+        font-size: 2rem;
+    }
+
+    @media (max-width: 700px) {
+        .logo img {
+            max-width: 90%;
+        }
+    }
+
+    @keyframes fadeInLeft {
+        from {
+            opacity: 0;
+            transform: translateX(-20px);
+        }
+
+        to {
+            opacity: 1;
+            transform: translateX(0);
+        }
+    }
+
+    .fadeInLeft {
+        animation: fadeInLeft 1s ease-out;
+    }
+
+    @keyframes fadeInRight {
+        from {
+            opacity: 0;
+            transform: translateX(50px);
+        }
+
+        to {
+            opacity: 1;
+            transform: translateX(0);
+        }
+    }
+
+    .fadeInRight {
+        animation: fadeInRightt 1s ease-out;
+    }
+</style>
+<section id="home" class="relative">
+    <div class="hero">
+
+        <div class="parallax-layer layer-6"></div>
+        <div class="parallax-layer layer-5"></div>
+        <div class="parallax-layer layer-4"></div>
+        <div class="parallax-layer bike-1"></div>
+        <div class="parallax-layer bike-2"></div>
+        <div class="parallax-layer bike-3"></div>
+        <div class="parallax-layer bike-4"></div>
+        <div class="parallax-layer layer-3"></div>
+        <div class="parallax-layer layer-2"></div>
+        <div class="parallax-layer layer-1"></div>
+        <div class="logo grid grid-cols-1 gap-4 place-items-center h-56 py-24">
+            <!-- <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/24650/logo.svg" alt="" /> -->
+            <div class="col">
+                <h1 class="text-bold">D & N</h1>
+            </div>
+            <div class="col mt-2 fadeInRight">
+                <h4 id="typing" class="text-black"></h4>
+            </div>
+        </div>
+        <div class="grid grid-cols-1 py-24 px-4">
+            <div class="col-span-full">
+                <a href="#rental" id="cta-hero" class="hero-cta py-1 px-1 pr-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800 fadeInLeft">
+                    <div class="flex justify-between items-center">
+                        <div class="absolute left-0 top-0 transform -translate-y-1/2 bg-blue-600 rounded-full text-white px-6 py-1.5 mr-24">
+                            <i class="fa-solid fa-van-shuttle fa-1xl"></i>
+                        </div>
+                        <div>
+                            <span id="typing-cta" class="text-lg font-medium">Selengkapnya &nbsp;&nbsp;<i class="fa-solid fa-angle-down text-lg"></i></span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </section>
-
-
-<script type="text/javascript">
-    $('#hero-cta').click(function(e) {
-        e.preventDefault(); 
-        let target = $(this).attr('href');
-        let offset = $(target).offset().top;
-        $('html, body').animate({
-          scrollTop: offset
-      }, 800);
-    });
-</script>
--- app/views/layout/AppLayout.php
+++ app/views/layout/AppLayout.php
@@ -1,79 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-     <style type="text/css" rel="stylesheet">
-      body.swal2-shown > [aria-hidden="true"] {
-        transition: 0.1s filter;
-        filter: blur(10px);
-      }
-    </style>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <style type="text/css" rel="stylesheet">
+    body.swal2-shown>[aria-hidden="true"] {
+      transition: 0.1s filter;
+      filter: blur(10px);
+    }
+  </style>
 
-     <!-- Meta google tag -->
-    <?php if($partials['meta_google_tag']): require_once  $partials['meta_google_tag']; endif;?>
+  <!-- Meta google tag -->
+  <?php if ($partials['meta_google_tag']) : require_once  $partials['meta_google_tag'];
+  endif; ?>
 
-    <?php if($partials['google_ads']): require_once $partials['google_ads']; endif;?>
+  <?php if ($partials['google_ads']) : require_once $partials['google_ads'];
+  endif; ?>
 
 
-    <script src="<?=$data['vendor']['tailwind']?>"></script>
-    <link href="<?=$data['vendor']['flowbite']['css']?>"  rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Poiret+One&family=Poppins:wght@900&family=Quicksand:wght@500&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href="/public/assets/css/style.css" />
-    <link rel="stylesheet" type="text/css" href="/public/assets/css/icons.css" />
+  <script src="<?= $data['vendor']['tailwind'] ?>"></script>
+  <link href="<?= $data['vendor']['flowbite']['css'] ?>" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Poiret+One&family=Poppins:wght@900&family=Quicksand:wght@500&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Aguafina+Script|Dancing+Script|Kaushan+Script|Open+Sans|Petit+Formal+Script|Pinyon+Script|Roboto|Rouge+Script" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="/public/assets/css/style.css" />
+  <link rel="stylesheet" type="text/css" href="/public/assets/css/icons.css" />
 
-    <script src="<?=$data['vendor']['sweetalert']?>"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  </style>
 
-    <link rel="canonical" href="<?=$meta['canonical']?>" />
-    <meta name="description" content="<?=$meta['meta_desc']?>">
-    <meta name="keywords" content="<?=$meta['meta_key']?>">
-    <meta name="author" content="<?=$meta['meta_author']?>">
-    <meta property="og:url" content="<?=$meta['og_url']?>">
-    <meta property="og:type" content="<?=$meta['og_type']?>" />
-    <meta property="og:site_name" content="<?=$meta['og_site_name']?>" />
-    <meta property="og:title" content="<?=$meta['og_title']?>">
-    <meta property="og:description" content="<?=$meta['og_desc']?>">
-    <meta property="og:image" content="<?=$meta['og_image']?>">
-    <meta property="og:image:width" content="<?=$meta['og_image_width']?>" />
-    <meta property="og:image:height" content="<?=$meta['og_image_height']?>" />
+  <script src="<?= $data['vendor']['sweetalert'] ?>"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <title><?=$meta['head_title']?></title>
+  <link rel="canonical" href="<?= $meta['canonical'] ?>" />
+  <meta name="description" content="<?= $meta['meta_desc'] ?>">
+  <meta name="keywords" content="<?= $meta['meta_key'] ?>">
+  <meta name="author" content="<?= $meta['meta_author'] ?>">
+  <meta property="og:url" content="<?= $meta['og_url'] ?>">
+  <meta property="og:type" content="<?= $meta['og_type'] ?>" />
+  <meta property="og:site_name" content="<?= $meta['og_site_name'] ?>" />
+  <meta property="og:title" content="<?= $meta['og_title'] ?>">
+  <meta property="og:description" content="<?= $meta['og_desc'] ?>">
+  <meta property="og:image" content="<?= $meta['og_image'] ?>">
+  <meta property="og:image:width" content="<?= $meta['og_image_width'] ?>" />
+  <meta property="og:image:height" content="<?= $meta['og_image_height'] ?>" />
 
-    <link rel="shortcut icon" href="<?=$data['favicon']?>">
+  <title><?= $meta['head_title'] ?></title>
 
-  </head>
+  <link rel="shortcut icon" href="<?= $data['favicon'] ?>">
+
+</head>
 
 
 <body class="flex flex-col min-h-screen dark:bg-gray-800">
-  
+
   <?php require_once $partials['navbar']; ?>
 
   <main class="bg-white dark:bg-gray-800 dark:text-white">
-    <?=$content; ?>
+    <?= $content; ?>
   </main>
 
-    
-    <section id="footer" class="container mx-auto px-4 py-24 dark:bg-gray-800">
-      <?php require_once 'partials/footer_content.php'; ?>
-    </section>
 
-    <script src="<?=$data['vendor']['flowbite']['js']?>"></script>
+  <section id="footer" class="container mx-auto px-4 py-24 dark:bg-gray-800">
+    <?php require_once 'partials/footer_content.php'; ?>
+  </section>
 
-    <script src="<?=$data['vendor']['contentful']?>"></script>
+  <script src="<?= $data['vendor']['flowbite']['js'] ?>"></script>
 
-    <script type="text/javascript">
-      let client = contentful.createClient({
-        space: '<?=$data['contentful_space'];?>',
-        accessToken: '<?=$data['contentful_token'];?>'
-      });
-    </script>
+  <script src="<?= $data['vendor']['contentful'] ?>"></script>
 
-    <?php for($i=0; $i < count($partials['scripts']); $i++): ?>
-      <script type="text/javascript" src="<?=$partials['scripts'][$i]?>"></script>
-    <?php endfor;?>
-    <script src="https://dashboard.chatfuel.com/integration/landing-wa-widget.js" async defer data-prefilled="Hello, DN Tour saya ingin memesan paket perjalanan dari DN Tour, bisa infokan saya harga terbaik dari DN Tour." data-welcome="Halo travelers, ada yang bisa kami bantu ?" data-phone="6283165539138"></script>
-  </body>
+  <script type="text/javascript">
+    let client = contentful.createClient({
+      space: '<?= $data['contentful_space']; ?>',
+      accessToken: '<?= $data['contentful_token']; ?>'
+    });
+  </script>
+
+  <?php for ($i = 0; $i < count($partials['scripts']); $i++) : ?>
+    <script type="text/javascript" src="<?= $partials['scripts'][$i] ?>"></script>
+  <?php endfor; ?>
+
+  <script src="https://dashboard.chatfuel.com/integration/landing-wa-widget.js" async defer data-prefilled="Hello, D & N Tour saya ingin memesan paket perjalanan dari D & N Tour, bisa infokan saya harga terbaik dari D & N Tour." data-welcome="Halo travelers, ada yang bisa kami bantu ?" data-phone="6283165539138"></script>
+</body>
+
 </html>
